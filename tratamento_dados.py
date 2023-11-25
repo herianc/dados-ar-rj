380bdae37ea2ffb1164a83db32ac8462fd25294c
Herian Cavalcante <herian.cavalcante@live.com>
2023-11-24 23:44:35 -0300

    att

--- tratamento_dados.py
+++ tratamento_dados.py
@@ -9,9 +9,7 @@ def tratando_dias(mes: int) -> int:
 
 
 def tratando_dados(lista: list, mes: int, ano: int) -> list:
-    """Função que trata os dados tirando os espaços em branco e convertendo os dados str para os tipos númericos (quando possível).
-    A estrutura da lista recebida muda com o ano da consulta"""
-    if ano > 2019:
+    if ano > 2019 or (ano == 2019 and mes == 12):
         for i in range(len(lista)-1):
             if lista[i] == 'ND' or lista[i] == 'NM':  # Poluente não medido no dia
                 lista[i] = 'NA'
@@ -25,38 +23,21 @@ def tratando_dados(lista: list, mes: int, ano: int) -> list:
 
             else:
                 lista[i] = int(lista[i])  # String de dígito inteiro
+    else:
+        for i in range(len(lista)-2):
 
-    elif ano <= 2019:
-        if ano == 2019 and mes == 12:  # Mês que MP2.5 foi adicionado ao site
-            for i in range(len(lista)-1):
-                if lista[i] == 'ND' or lista[i] == 'NM':  # Poluente não medido no dia
-                    lista[i] = 'NA'
+            if lista[i] == 'ND' or lista[i] == 'NM':  # Poluente não medido no dia
+                lista[i] = 'NA'
 
-                elif ',' in lista[i]:  # Dados decimais
-                    lista[i] = (lista[i].replace(',', '.'))
-                    lista[i] = float(lista[i])
+            elif ',' in lista[i]:  # Dados decimais
+                lista[i] = (lista[i].replace(',', '.'))
+                lista[i] = float(lista[i])
 
-                elif i == len(lista) - 1:  # Classificação Ex: Bom ou Péssimo
-                    continue
+            elif i == len(lista) - 1:  # Classificação Ex: Bom ou Péssimo
+                continue
 
-                else:
-                    lista[i] = int(lista[i])  # String de digito inteiro
-        else:
-            for i in range(len(lista)-2):
-
-                if lista[i] == 'ND' or lista[i] == 'NM':  # Poluente não medido no dia
-                    lista[i] = 'NA'
-
-                elif ',' in lista[i]:  # Dados decimais
-                    lista[i] = (lista[i].replace(',', '.'))
-                    lista[i] = float(lista[i])
-
-                elif i == len(lista) - 1:  # Classificação Ex: Bom ou Péssimo
-                    continue
-
-                else:
-                    lista[i] = int(lista[i])  # String de digito inteiro
-
+            else:
+                lista[i] = int(lista[i])  # String de digito inteiro
     return lista
 
 
@@ -79,7 +60,7 @@ def estacao_indisponivel() -> dict:
 def criando_dicionario(lista: list, mes, ano) -> dict:
     '''Função que cria dicionário de acordo com o ano da consulta'''
     dicionario = {}
-    if ano >= 2020:
+    if ano >= 2020 or (ano == 2019 and mes == 12):
         dicionario['MP10'] = lista[0]
         dicionario['MP2.5'] = lista[1]
         dicionario['O3'] = lista[2]
@@ -89,26 +70,15 @@ def criando_dicionario(lista: list, mes, ano) -> dict:
         dicionario['IQAr'] = lista[6]
         dicionario['classificacao'] = lista[7]
 
-    elif ano <= 2019:
-        if ano == 2019 and mes == 12:
-            dicionario['MP10'] = lista[0]
-            dicionario['MP2.5'] = lista[1]
-            dicionario['O3'] = lista[2]
-            dicionario['CO'] = lista[3]
-            dicionario['NO2'] = lista[4]
-            dicionario['SO2'] = lista[5]
-            dicionario['IQAr'] = lista[6]
-            dicionario['classificacao'] = lista[7]
-
-        else:
-            dicionario['MP10'] = lista[0]
-            dicionario['MP2.5'] = 'NA'  # MP2.5 não era medido
-            dicionario['O3'] = lista[1]
-            dicionario['CO'] = lista[2]
-            dicionario['NO2'] = lista[3]
-            dicionario['SO2'] = lista[4]
-            dicionario['IQAr'] = lista[5]
-            # Entre 2017 a 11/2019 os dados
-            dicionario['classificacao'] = lista[-2]
+    else:
+        dicionario['MP10'] = lista[0]
+        dicionario['MP2.5'] = 'NA'  # MP2.5 não era medido
+        dicionario['O3'] = lista[1]
+        dicionario['CO'] = lista[2]
+        dicionario['NO2'] = lista[3]
+        dicionario['SO2'] = lista[4]
+        dicionario['IQAr'] = lista[5]
+        # Entre 2017 a 11/2019 a quantidade de colunas era menor
+        dicionario['classificacao'] = lista[-2]
 
     return dicionario
